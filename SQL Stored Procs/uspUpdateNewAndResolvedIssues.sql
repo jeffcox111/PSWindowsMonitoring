ccf8e5d6f3dd929d8395551b867b59fd7f42d980
Jeff Cox <jeffcox111@gmail.com>
2016-12-08 13:37:56 -0600

    Restored lost script and added DB table and sproc scripts.

---
+++ SQL Stored Procs/uspUpdateNewAndResolvedIssues.sql
@@ -0,0 +1,161 @@
+﻿-- =============================================
+-- Author:		Jeff Cox
+-- Create date: 8/28/2015
+-- Description:	Update Issues table with new and resolved issues
+-- ***Schedule this proc to be called by a SQL Job every 5 mintues***
+-- =============================================
+CREATE PROCEDURE [dbo].[uspUpdateNewAndResolvedIssues]
+AS
+BEGIN
+
+	-- Add new Issues----------------------------------
+	create table #newLogEntries (
+	   id bigint IDENTITY(1,1) NOT NULL,
+	   server varchar(50),
+	   monitoringtype varchar(100),
+	   errormessage varchar(500),
+	   timestamp datetime2
+	   )
+
+	insert into #newLogEntries select Server, MonitoringType, ErrorMessage, TimeStamp from logentries
+		where isheartbeat = 0 and timestamp between  dateadd(mi, -5, getdate()) and getdate()
+
+
+	declare @iterator int = 1
+	declare @count int
+	select @count = count(1) from #newLogEntries
+
+	while(@count >= @iterator)
+	begin
+		if(exists(select nle.* from #newlogentries nle 
+			join issues i on nle.server = i.server and nle.monitoringtype = i.monitoringtype
+			where nle.id = @iterator and i.endtime is null))
+		begin
+			--issue is already being tracked, so delete it from staging table of new issues
+			delete from #newlogentries where id = @iterator
+		end
+
+		set @iterator = @iterator + 1
+	end
+
+	--anything remaining in #newLogEntries will be a new Issue and needs to be added
+    INSERT INTO Issues (Server, MonitoringType, ErrorMessage, StartTime, SendStartEmail, SendEndEmail)
+	SELECT server, monitoringtype, errormessage, timestamp, 1, 1 FROM #newLogEntries
+
+	drop table #newLogEntries
+	--/end Add new Issues---------------------------------
+	
+	--Update resolved issues-------------------------------
+	create table #currentIssues (
+	  tempId bigint identity(1,1) not null,
+	  id bigint,
+	  server varchar(50),
+	  monitoringtype varchar(100),
+	  errormessage varchar(500),
+	  starttime datetime2,
+	  endtime datetime2 null
+	  )
+
+	  insert into #currentIssues select id, server, monitoringtype, errormessage, starttime, null from issues
+	   where endtime is null 
+
+	
+	select @count = count(1) from #currentIssues
+	set @iterator = 1
+
+	while(@count >= @iterator)
+	begin
+		if(not exists(select * from logentries le join #currentIssues ci on le.server = ci.server and le.MonitoringType = ci.monitoringtype and ci.tempid = @iterator
+			having max(le.timestamp) > dateadd(mi, -5, getdate())))
+		begin
+			--issue has been resolved so update with endtime
+			update Issues  -- set endtime = getdate()
+			set endtime = (select max(le.timestamp) from logentries le join #currentIssues ci on le.server = ci.server and le.MonitoringType = ci.monitoringtype and ci.tempid = @iterator)
+			where id = (select id from #currentIssues where tempid = @iterator)
+		end
+
+		set @iterator = @iterator + 1
+	end
+
+	drop table #currentissues
+	--/end Update Resolved Issues---------------------------
+
+	declare @Body varchar(max),
+				@TableHead varchar(max),
+				@TableTail varchar(max),
+				@MessageHeader varchar(max),
+				@Footer varchar(max)
+
+	if(exists(select * from issues where SendStartEmail = 1))
+	begin
+
+		SET @MessageHeader = 'Current System Issues<br><br>';
+		SET @Footer = ''
+		SET @TableTail = '</table></body></html>';
+		SET @TableHead = '<html><head>' +
+                  '<style>' +
+                  'td {border: solid black 1px;padding-left:5px;padding-right:5px;padding-top:1px;padding-bottom:1px;font-size:11pt;} ' +
+                  '</style>' +
+                  '</head>' +
+                  '<body><table cellpadding=0 cellspacing=0 border=0>' +
+                  '<tr bgcolor=#e86363><td align=center><b>Error Messages</b></td></tr>';
+		SELECT @Body = (SELECT ErrorMessage from monitoring..issues where SendStartEmail = 1 for XML raw('tr'), Elements)
+		SET @Body = @MessageHeader + @TableHead + @Body + @TableTail + @Footer;
+
+		IF EXISTS(SELECT TOP 1 1 FROM Settings WHERE SettingName = 'SendMonitoringAlertEmail' AND SettingValue = '1')
+		BEGIN
+			EXEC msdb.dbo.sp_send_dbmail
+				@profile_name = 'Monitoring',
+				@recipients = '',
+				@body = @Body,
+				@body_format = 'HTML',
+				@subject = 'NEW System Issue(s)' ;
+		END
+
+		UPDATE Issues set SendStartEmail = 0
+	end
+
+
+	if(exists(select * from issues where SendEndEMail = 1 and endtime is not null ))
+	begin
+
+		SET @MessageHeader = 'RESOLVED System Issues<br><br>';
+		SET @Footer = ''
+		SET @TableTail = '</table></body></html>';
+		SET @TableHead = '<html><head>' +
+                  '<style>' +
+                  'td {border: solid black 1px;padding-left:5px;padding-right:5px;padding-top:1px;padding-bottom:1px;font-size:11pt;} ' +
+                  '</style>' +
+                  '</head>' +
+                  '<body><table cellpadding=0 cellspacing=0 border=0>' +
+                  '<tr bgcolor=#8FDB20><td align=center><b>Error Messages</b></td></tr>';
+		SELECT @Body = (SELECT ErrorMessage from monitoring..issues where SendEndEMail = 1 and endtime is not null for XML raw('tr'), Elements)
+		SET @Body = @MessageHeader + @TableHead + @Body + @TableTail + @Footer;
+
+		IF EXISTS(SELECT TOP 1 1 FROM Settings WHERE SettingName = 'SendMonitoringAlertEmail' AND SettingValue = '1')
+		BEGIN
+			EXEC msdb.dbo.sp_send_dbmail
+				@profile_name = 'Monitoring',
+				@recipients = '',
+				@body = @Body,
+				@body_format = 'HTML',
+				@subject = 'RESOLVED System Issue(s)' ;
+		END
+
+		UPDATE Issues set SendEndEmail = 0 where SendEndEmail = 1 and EndTime is not null
+	end
+
+	
+	DECLARE @monitoringdisableddate datetime
+	DECLARE @dayssinceupdate int
+
+	SELECT @monitoringdisableddate=LastSetTimeStamp FROM Settings WHERE SettingName = 'SendMonitoringAlertEmail' AND SettingValue = 0
+	select @monitoringdisableddate = ISNULL(@monitoringdisableddate, GETDATE())
+	select @Dayssinceupdate = DATEDIFF(DD,@monitoringdisableddate, GETDATE())
+
+	IF @dayssinceupdate > 0
+		UPDATE Settings Set SettingValue = '1', LastSetTimeStamp=GETDATE(), LastSetBy='SQL' where SettingName = 'SendMonitoringAlertEmail'
+
+
+
+END
--- WindowsHelpers.ps1
+++ WindowsHelpers.ps1
@@ -1,3 +1,219 @@
+Add-Type -Language CSharp @"
+public class LogEntry{
+    public System.DateTime TimeStamp;
+    public string Server;
+    public string MonitorType;
+    public string ErrorMessage;
+    public bool IsHeartbeat;
+}
+"@;
+
+
+function Check-SQLBlocking([string]$serverName)
+{
+    write-host "Looking for blocking in DB $serverName ..."
+    $sqlconn = New-Object System.Data.SqlClient.SqlConnection
+    $sqlconn.ConnectionString = "Server=" + $serverName + ";Database=master;Integrated Security=SSPI;"
+    $sqlconn.Open()
+
+    $sqlcommand = $sqlconn.CreateCommand()
+    $sqlcommand.CommandText = "SELECT  count(1) FROM master.dbo.sysprocesses sp JOIN master.dbo.sysdatabases sd ON sp.dbid = sd.dbid and sp.blocked > 0"
+    $result = $sqlcommand.ExecuteScalar()
+    $sqlconn.Close()
+
+    if($result -gt 0)
+    {              
+       $logentry = new-object LogEntry
+       $logentry.Server = $serverName
+       $logentry.TimeStamp = [DateTime]::Now
+       $logentry.MonitorType = "Check-SQLBlocking"
+       $logentry.ErrorMessage = "There is SQL blocking taking place in " + $serverName + "."
+       
+       write-host $logentry.ErrorMessage
+       return $logentry       
+    }    
+}
+
+function Check-ServerIsOnline([string]$serverName, [string]$friendlyHostName = "")
+{
+    if($friendlyHostName -eq "")
+    {
+        $friendlyHostName = $serverName
+    }
+    else 
+    {
+        $friendlyHostName = $friendlyHostName + " (" + $serverName + ")"
+    }
+
+    write-host "Testing connectivity with $friendlyHostName..."
+    if (-Not (Test-Connection $serverName -Count 2 -ErrorAction SilentlyContinue))
+    {
+        $logentry = new-object LogEntry
+        $logentry.Server = $friendlyHostName
+        $logentry.TimeStamp = [DateTime]::Now
+        $logentry.MonitorType = "Check-ServerIsOnline"
+        $logentry.ErrorMessage = "Cannot connect to $friendlyHostName."
+
+        write-host $logentry.ErrorMessage
+        return $logentry
+    }     
+}
+
+function Check-MonitoringHostRebooted()
+{
+   $os = Get-WmiObject win32_operatingsystem
+   $uptime = (Get-Date) - ($os.ConvertToDateTime($os.lastbootuptime))
+   $Display = "Checking host uptime... " + $Uptime.Days + " days, " + $Uptime.Hours + " hours, " + $Uptime.Minutes + " minutes" 
+   write-host $display
+   if($uptime.days -eq 0 -and $uptime.hours -eq 0 -and $uptime.minutes -gt 10 -and $uptime.minutes -lt 15)
+   {
+      $logentry = new-object LogEntry
+      $logentry.Server = $env:computername
+      $logentry.TimeStamp = [DateTime]::Now
+      $logentry.MonitorType = "Check-MonitoringHostRebooted"
+      $logentry.ErrorMessage = "The host of this monitoring script ($env:computername) has successfully rebooted."
+      
+      write-host $logentry.ErrorMessage
+      return $logentry
+   }
+   
+}
+
+function Check-Freespace([string] $serverName,[string] $drive, [int]$thresholdGigs)
+{
+    if (Test-Connection $serverName -Count 1 -ErrorAction SilentlyContinue)
+    {
+        if((Get-WmiObject Win32_LogicalDisk -ComputerName $serverName -Filter "DeviceID='$drive'") -ne $null)
+        {
+          $freespace = (Get-WmiObject Win32_LogicalDisk -ComputerName $serverName -Filter "DeviceID='$drive'" | Select-Object FreeSpace).FreeSpace / 1024 / 1024 / 1024
+          $Display = "Checking free space on $serverName $drive ..."
+          write-host $display
+
+          $logentry = new-object LogEntry
+          $logentry.Server = $serverName
+          $logentry.TimeStamp = [DateTime]::Now
+          $logentry.MonitorType = "Check-Freespace"
+
+          if ($freespace -eq 0)
+          {
+              $logentry.ErrorMessage = "Drive $drive on $serverName is out of space."
+              write-host $logentry.ErrorMessage
+              return $logentry
+              
+          }
+          elseif($freespace -lt $thresholdGigs)
+          {
+              $mb = "{0:N2}" -f ($freespace * 1024)
+              $logentry.ErrorMessage = "Drive $drive on $serverName is below $thresholdGigs gig of free space. There are $mb MB remaining."
+              write-host $logentry.ErrorMessage
+              return $logentry            
+          }
+        }
+        else
+        {
+            $logentry = new-object LogEntry
+            $logentry.Server = $serverName
+            $logentry.TimeStamp = [DateTime]::Now
+            $logentry.MonitorType = "Check-Freespace"
+            $logentry.ErrorMessage = "Can't currently perform WMI queries against $serverName."
+            
+            write-host $logentry.ErrorMessage
+            return $logentry
+        }
+    }
+}
+
+function Check-WebsiteOnline([string]$url, [string]$friendlyName)
+{
+    write-host "Checking website $friendlyName ..."
+    $statusCode = ""
+    $request = $null 
+    try
+    {
+       $request = Invoke-WebRequest -Uri $url -TimeoutSec 20 -Method head 
+       $statusCode = $request.StatusCode
+    }  
+    catch 
+    { 
+       $request = $_.Exception.Response 
+       $statusCode = $request.StatusCode
+    }  
+
+    if(-Not($statusCode -eq 200 -or $statusCode -eq "Unauthorized"))
+    {
+       $logentry = new-object LogEntry
+       $logentry.Server = $friendlyName
+       $logentry.TimeStamp = [DateTime]::Now
+       $logentry.MonitorType = "Check-WebsiteOnline"
+       $logentry.ErrorMessage = "$friendlyName is offline or unreachable."
+
+       write-host $logentry.ErrorMessage
+       return $logentry
+    }
+}
+
+function Check-DBEmailFailures([string] $server)
+{
+    write-host "Checking for failed emails on $server..."
+    $sqlConn = New-Object System.Data.SqlClient.SqlConnection
+    $sqlConn.ConnectionString = "Data Source=$server;Initial Catalog=Monitoring;Persist Security Info=True;User ID=;Password="
+    $sqlConn.Open()
+
+    $sql = "select count(1) from msdb..sysmail_allitems where sent_status <> 'sent' and last_mod_date between dateadd(mi, -5, getdate()) and getdate()"
+    $cmd = New-Object System.Data.SqlClient.SqlCommand($sql,$sqlConn)
+    $count = $cmd.ExecuteScalar()
+
+    if($count -gt 0)
+    {
+    
+       $logentry = new-object LogEntry
+       $logentry.Server = $server
+       $logentry.TimeStamp = [DateTime]::Now
+       $logentry.MonitorType = "Check-DBEmailFailure"
+       $logentry.ErrorMessage = "$count email(s) failed being sent from $server."
+
+       write-host $logentry.ErrorMessage
+       return $logentry
+    }
+}
+
+function Check-UsedDiskSpaceLinux([string]$server, [string]$partition, [int]$thresholdPercentage)
+{
+    if (Test-Connection $server -Count 1 -ErrorAction SilentlyContinue)
+    {
+        write-host "Checking free drive space on server: $server, file system: $partition..."
+        $x = . 'c:\program files\git\usr\bin\ssh.exe' $server -l tproot -i privatekey.ppk "df | grep $partition | awk '{print `$4}'"
+
+        if($x -eq $null)
+        {
+     
+          $logentry = new-object LogEntry
+          $logentry.Server = $server
+          $logentry.TimeStamp = [DateTime]::Now
+          $logentry.MonitorType = "Check-UsedDiskSpaceLinux"
+          $logentry.ErrorMessage = "The partition $partition on server $server does not exist."
+      
+          write-host $logentry.ErrorMessage
+          return $logentry
+        }
+
+        $usedSpacePercent = $x.replace("%", "")
+       
+
+        if([int]$usedSpacePercent -gt $thresholdPercentage)
+        {
+          $logentry = new-object LogEntry
+          $logentry.Server = $server
+          $logentry.TimeStamp = [DateTime]::Now
+          $logentry.MonitorType = "Check-UsedDiskSpaceLinux"
+          $logentry.ErrorMessage = "On the server $server, the partition $partition is at $usedSpacePercent % capacity and should be below $threshholdPercentage %."
+      
+          write-host $logentry.ErrorMessage
+          return $logentry
+        }
+    }
+}
+
 function Get-LogonHistory{
     Param (
      [string]$Computer = 'localhost', #(Read-Host Remote computer name),
@@ -20,6 +236,7 @@ function Get-LogonHistory{
             { 
                 $ET = "Logoff"
             }
+
             Else
             { 
                 Continue
@@ -31,7 +248,7 @@ function Get-LogonHistory{
                 User = (New-Object System.Security.Principal.SecurityIdentifier $Log.ReplacementStrings[1]).Translate([System.Security.Principal.NTAccount])
             }
         }
- 
+         
         $Result | Select User, Time | Sort Time #| Out-GridView
     }
 }
