1eb097bac93cadee9d0b22f79ed32dc243c7db27
Unknown <jeffcox111@gmail.com>
2018-01-25 08:36:57 -0600

    Added missing Settings table.

---
+++ Tables/Settings.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Settings] (
+    [ID]             BIGINT         IDENTITY (1, 1) NOT NULL,
+    [SettingName]    VARCHAR (100)  NOT NULL,
+    [SettingValue]   VARCHAR (100)  NOT NULL,
+    CONSTRAINT [PK_Settings] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+
