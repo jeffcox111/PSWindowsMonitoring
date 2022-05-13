36fab49269189d49b54df6d95eaf7096d670c351
Jeff Cox <jeffcox111@gmail.com>
2022-05-13 14:59:47 -0500

    Refactored so that the user-modified script only holds the one main function

--- PSMonitoring.ps1
+++ PSMonitoring.ps1
@@ -2,15 +2,7 @@
 . .\Define-Classes.ps1
 . .\MonitoringChecks.ps1
 . .\SupportFunctions.ps1
-function Run-MonitoringChecks ($notifications = $true)
-{    
-    #Run monitoring checks...
-    #This is where you come in!  
-    #Add lines below this comment block to check on the things you want to monitor.
-    Check-ServerIsOnline google.com 
-    Check-Freespace localhost C: 100 
-    Check-ProcessRunningThatShouldNotBe notepad $true
-} 
+. .\User-CustomMonitoring.ps1
 
 $settings = Load-Settings
 $messages = New-Object Collections.Generic.List[LogEntry]
